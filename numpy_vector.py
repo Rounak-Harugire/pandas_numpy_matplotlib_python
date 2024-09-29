dd67f85f86b65701fc73fdc8cf5d011fe39569cb
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-09-29 22:25:20 +0530

    Add files via upload

---
+++ numpy_vector.py
@@ -0,0 +1,12 @@
+import numpy as np 
+list1 = [1, 2, 3] 
+list2 = [[1], 
+		[2], 
+		[3]] 
+vect_1 = np.array(list1) 
+vect_2 = np.array(list2) 
+print("Horizontal Vector") 
+print(vect_1) 
+print("*******************") 
+print("Vertical Vector") 
+print(vect_2) 
